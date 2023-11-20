d0d7f123ca27b3f09e968babe50ec678d92576d1
sonico <115878416+sonico5206@users.noreply.github.com>
2023-11-20 12:05:18 +0300

    Add files via upload

---
+++ lab_lin_signal.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+void signalHandler(int signal) {
+    std::cout << "Received signal: " << signal << std::endl;
+    exit(signal);
+}
+
+int main() {
+    signal(SIGINT, signalHandler);
+
+    std::cout << "Waiting for signal..." << std::endl;
+
+    while (true) {
+        sleep(1);
+    }
+
+    return 0;
+}
